f3c95659beb0324a999ef9287f844f11f2b02f77
Nambyungwook <skaquddnr21@gmail.com>
2024-08-13 16:07:41 +0900

    [single-module] unused import 제거

--- app/src/main/java/kr/co/nbw/composesearchapp/feature/search/SearchScreen.kt
+++ app/src/main/java/kr/co/nbw/composesearchapp/feature/search/SearchScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
